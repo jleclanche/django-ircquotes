0f5423cb5e837c9276bdf12aff0fdf0d2728d280
Jerome Leclanche <jerome@leclan.ch>
2010-06-18 01:19:54 +0300

    Style up the quotes and add all the info

--- urls.py
+++ urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns("",
 	(r"^admin/doc/", include("django.contrib.admindocs.urls")),
 	(r"^admin/", include(admin.site.urls)),
+	(r"^static/(?P<path>.*)$", "django.views.static.serve", {"document_root": "/home/adys/src/git/ircquotes/static"}),
 	(r"^", include("ircquotes.quotes.urls")),
 )
