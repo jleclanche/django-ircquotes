9bdfb9b4803230e0be0efcaa26bb60ec4b23bfd2
Jerome Leclanche <jerome@leclan.ch>
2012-02-11 19:14:02 +0000

    Use local_settings instead of settings_override, update settings

--- settings.py
+++ settings.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Django settings for ircquotes project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	("Adys", "adys.wh@gmail.com")
+	("Jerome Leclanche", "adys.wh@gmail.com")
 )
 
 MANAGERS = ADMINS
@@ -52,9 +51,9 @@ SECRET_KEY = "w0(vt01fc(%go*hgpnv2e7i1)zq!j)atd8y$w)sgu!2i(6b(n8"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+#	'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,18 +71,49 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	#'django.contrib.sites',
-	'django.contrib.messages',
-	'django.contrib.admin',
-	'django.contrib.admindocs',
+	"django.contrib.auth",
+	"django.contrib.contenttypes",
+	"django.contrib.sessions",
+	"django.contrib.sites",
+	"django.contrib.messages",
+	"django.contrib.staticfiles",
+	# Uncomment the next line to enable the admin:
+	"django.contrib.admin",
+	# Uncomment the next line to enable admin documentation:
+	"django.contrib.admindocs",
 	"ircquotes.quotes",
 )
 
-# Unversioned local overrides
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+	"version": 1,
+	"disable_existing_loggers": False,
+	"filters": {
+		"require_debug_false": {
+			"()": "django.utils.log.RequireDebugFalse"
+		}
+	},
+	"handlers": {
+		"mail_admins": {
+			"level": "ERROR",
+			"filters": ["require_debug_false"],
+			"class": "django.utils.log.AdminEmailHandler"
+		}
+	},
+	"loggers": {
+		"django.request": {
+			"handlers": ["mail_admins"],
+			"level": "ERROR",
+			"propagate": True,
+		},
+	}
+}
+
 try:
-	from settings_override import *
+	from local_settings import *
 except ImportError:
 	pass
